b1f31285323f1f684ba352156f8874cd1255d6a0
Jesus <jd.pinilla@uniandes.edu.co>
2021-07-15 20:55:46 -0500

    [add] Collections

--- src/components/Collections/index.jsx
+++ src/components/Collections/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+import AppContext from '../../context/AppContext';
+
 const Collections = () => {
   return <div></div>;
 };
