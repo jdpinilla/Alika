37a7a00918ed306655b479f754470fd9be456e47
unknown <jd.pinilla@uniandes.edu.co>
2021-07-17 01:04:37 -0500

    [add] Collections with Carousel

--- src/components/Collections/index.jsx
+++ src/components/Collections/index.jsx
@@ -1,39 +1,60 @@
 import React, { useContext } from 'react';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
 import Collection from '../Collection';
 import AppContext from '../../context/AppContext';
+import Grid from '@material-ui/core/Grid';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import '../styles/Collections.css';
-const useStyles = makeStyles(() => ({
-  root: {
-    flexGrow: 1,
-    maxWidth: 1100,
-  },
-}));
+import Slider from 'react-slick';
 
+const settings = {
+  dots: true,
+  focusOnSelect: true,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  speed: 2000,
+  autoplaySpeed: 4000,
+  cssEase: 'linear',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
 const Collections = () => {
   const { state } = useContext(AppContext);
   const { categories } = state;
 
-  const classes = useStyles();
   return (
     <>
-      <div className="Collections">
-        <div className={classes.root}>
-          <Grid
-            container
-            spacing={0}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            {categories.map((category) => (
-              <Grid key={category.id} item xs={12} sm={6} md={4}>
-                <Collection category={category} />{' '}
-              </Grid>
-            ))}
-          </Grid>
-        </div>
+      <div className="Carousel">
+        <Slider {...settings}>
+          {categories.map((category) => (
+            <Collection key={category.id} category={category} />
+          ))}
+        </Slider>
       </div>
     </>
   );
